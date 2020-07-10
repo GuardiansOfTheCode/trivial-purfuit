516f88d454667dfa538890882042d10a298645e7
Trevor Jones <trevorjones@Trevors-MBP.fios-router.home>
2020-07-10 11:52:53 -0400

    Reformatted code

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+            <p>This is a paragraph</p>
+        </div>
+      // The above code (not true HTML) is compiled to the following:
+      // React.createElement('div',
+      //     {className: 'App'},
+      //     React.createElement('h1', null, 'Hello')
+      // )
+    );
 }
 
 export default App;
