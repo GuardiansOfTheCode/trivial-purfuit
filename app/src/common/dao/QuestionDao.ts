f21eebccb8faf292626de2113def44da3ad59d08
Trevor Jones <trevor.c.jones87@gmail.com>
2020-07-15 19:03:07 -0400

    Added die roll, updated formatting

--- app/src/common/dao/QuestionDao.ts
+++ app/src/common/dao/QuestionDao.ts
@@ -1,14 +1,16 @@
 import {Question} from '../models/Question';
 
 export class QuestionDao {
+    private static _instance: QuestionDao = new QuestionDao();
+    // TODO: Remove/edit after skeletal increment demo
+    private _demoDB: Question[] = [];
+
     constructor() {
         if (QuestionDao._instance) {
             throw new Error('Instantiation failed: Use QuestionDao.instance instead of \'new\'.');
         }
         QuestionDao._instance = this;
     }
-
-    private static _instance: QuestionDao = new QuestionDao();
 
     static get instance(): QuestionDao {
         return this._instance;
@@ -19,9 +21,6 @@ export class QuestionDao {
     }
 
     // TODO: Remove/edit after skeletal increment demo
-    private _demoDB: Question[] = [];
-
-    // TODO: Remove/edit after skeletal increment demo
     get demoDB(): Question[] {
         return this._demoDB;
     }
@@ -30,5 +29,23 @@ export class QuestionDao {
     insert(question: Question) {
         console.log(`[QuestionDao] insert(${JSON.stringify(question)})`);
         this._demoDB.push(question);
+    }
+
+    // TODO: Remove/edit after skeletal increment demo
+    delete() {
+        console.log(`[QuestionDao] delete()`);
+        this._demoDB.pop();
+    }
+
+    // TODO: Remove/edit after skeletal increment demo
+    printDemoDB(): void {
+        console.log(`[QuestionDao] printDemoDB: SIZE ${this._demoDB.length} CONTENTS ${JSON.stringify(this._demoDB)}`);
+    }
+
+    // TODO: Remove/edit after skeletal increment demo
+    getFirstItem(): Question {
+        const firstItem: Question = this._demoDB[0];
+        console.log(`[QuestionDao] getFirstItem(): ${JSON.stringify(firstItem)}`);
+        return firstItem;
     }
 }
--- app/src/components/StartScreen/Players/Player/Player.tsx
+++ app/src/components/StartScreen/Players/Player/Player.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import {Grid} from '@material-ui/core';
-import './Player.css';
 import propTypes from 'prop-types';
+import React from 'react';
+import './Player.css';
 
 const Player = (props: any) => {
     return (
