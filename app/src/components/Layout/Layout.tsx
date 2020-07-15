5786ce818fdd45a35255c153f9ad213bf935fd33
Trevor Jones <54898191+trevor-c-jones@users.noreply.github.com>
2020-07-14 22:23:24 -0400

    trevor-implement-question-component (#8)
    * Added Box wrapper for better formatting
    
    * Added TODOs
    
    * Updated position to use x,y coordinates
    
    * Removed unused state
    
    * Added cakeSlice attributes to GameSpace components
    
    * Updated game board
    
    * Updated board

--- app/src/components/Layout/Layout.tsx
+++ app/src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 
+// TODO: Add navbar for use over entire application
 const Layout = (props: any) => {
     return (
         <Auxiliary>
