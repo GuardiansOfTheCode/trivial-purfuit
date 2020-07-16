57dbac890c2bc107a0090eca7649639d20b3e264
Trevor Jones <54898191+trevor-c-jones@users.noreply.github.com>
2020-07-15 21:44:26 -0400

    Refactor, updated formatting again (#16)

--- app/src/common/dao/QuestionDao.ts
+++ app/src/common/dao/QuestionDao.ts
@@ -1,9 +1,5 @@
-import {Question} from '../models/Question';
-
 export class QuestionDao {
     private static _instance: QuestionDao = new QuestionDao();
-    // TODO: Remove/edit after skeletal increment demo
-    private _demoDB: Question[] = [];
 
     constructor() {
         if (QuestionDao._instance) {
@@ -18,34 +14,5 @@ export class QuestionDao {
 
     static set instance(value: QuestionDao) {
         this._instance = value;
-    }
-
-    // TODO: Remove/edit after skeletal increment demo
-    get demoDB(): Question[] {
-        return this._demoDB;
-    }
-
-    // TODO: Remove/edit after skeletal increment demo
-    insert(question: Question) {
-        console.log(`[QuestionDao] insert(${JSON.stringify(question)})`);
-        this._demoDB.push(question);
-    }
-
-    // TODO: Remove/edit after skeletal increment demo
-    delete() {
-        console.log(`[QuestionDao] delete()`);
-        this._demoDB.pop();
-    }
-
-    // TODO: Remove/edit after skeletal increment demo
-    printDemoDB(): void {
-        console.log(`[QuestionDao] printDemoDB: SIZE ${this._demoDB.length} CONTENTS ${JSON.stringify(this._demoDB)}`);
-    }
-
-    // TODO: Remove/edit after skeletal increment demo
-    getFirstItem(): Question {
-        const firstItem: Question = this._demoDB[0];
-        console.log(`[QuestionDao] getFirstItem(): ${JSON.stringify(firstItem)}`);
-        return firstItem;
     }
 }
