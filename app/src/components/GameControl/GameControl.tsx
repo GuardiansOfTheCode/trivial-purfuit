063e4f5cb3d70f61f7d9aa2137823b92e8a6b858
Trevor Jones <trevor.c.jones87@gmail.com>
2020-08-05 14:52:53 -0400

    Removed StartScreen component

--- app/src/components/GameControl/GameControl.tsx
+++ app/src/components/GameControl/GameControl.tsx
@@ -1,6 +1,7 @@
 import {Button, Grid, Paper} from '@material-ui/core';
 import propTypes from 'prop-types';
 import React from 'react';
+import Players from '../Players/Players';
 
 /**
  * This component contains game state information such as player names, token cake status, and current player turn.
@@ -14,7 +15,9 @@ const GameControl = (props: any) => {
         <Paper>
             <Grid container>
                 <Grid item xs={12}>
-                    <h1>Game Control</h1>
+                    <h1>Trivial Purfuit</h1>
+                    <Players players={props.players}
+                             changeName={props.changeName}/>
                 </Grid>
                 <Grid item xs={12}>
                     <Button variant={'contained'} color={'primary'}>
@@ -28,6 +31,8 @@ const GameControl = (props: any) => {
 
 GameControl.propTypes = {
     players: propTypes.array,
+    changeName: propTypes.func,
+    startGame: propTypes.func
 };
 
 export default GameControl;
