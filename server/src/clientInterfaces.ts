870ac43c2eb3d0e707e5b1ebc2f0cd483c844102
Trevor Jones <54898191+trevor-c-jones@users.noreply.github.com>
2020-08-04 20:42:50 -0400

    henrySandbox (#20)
    * Added server implementation for Question and Answer database
    
    * further changes
    
    * Set the default questions for demo
    
    Co-authored-by: Henry Acquah <hacquah24@gmail.com>

---
+++ server/src/clientInterfaces.ts
@@ -0,0 +1,23 @@
+export interface QuestionCard
+{   
+   id: number
+   category: Category
+   question: string,
+   answers: Choices[]
+}
+
+export interface Choices
+{   
+   id: number
+   answer: string
+   correct: boolean //should be set when needed to be true
+   questionId?: number,
+}
+
+export enum Category {
+   RED,
+   BLUE,
+   GREEN,
+   WHITE,
+   END_CATEGORY
+}
---
+++ server/src/db/dbConfig.ts
@@ -0,0 +1,10 @@
+export const dbName: string = "database.sqlite3";
+export const tableQuestions: string = 'questions';
+export const tableAnswers: string = 'answers';
+
+export const colIdStr: string =".id"
+export const colCategoryStr: string = ".category"
+export const colQuestionStr: string = ".question"
+export const colAnswerStr: string = ".answer"
+export const colCorrectStr: string = ".correct"
+export const colQuestionIdStr: string = ".questionId" 
